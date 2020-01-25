448f34d7275ceff101fdc916448721c5818d86bc
Tran Phuoc Hung <admin@tygon.com.vn>
2020-01-25 15:57:37 +0700

    demo Props and State

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return (
+        <Text style={styles.textgreatings}>{this.props.text1}</Text>
+      );
+    }
+
+    return (
+      <Text style={styles.textgreatings}>{this.props.text}</Text>
+    );
+  }
+}
+class Greeting extends Component {
+  render() {
+    return (
+        <Text style={styles.textgreatings}>Xin chào {this.props.name}!</Text>
+    );
+  }
+}
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{ width: 193, height: 110 }} />
+    );
+  }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Bananas pic='123' />
+      <Text style={styles.text}>Xin chào React Native</Text>
+      <Blink text='Đây là ba Hùng' text1='Ba chào các con!'/>
+      <Greeting name='Mộc Mộc' />
+      <Greeting name='Viên Viên' />
     </View>
   );
 }
@@ -12,8 +59,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#07c',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 17,
+  },
+  textgreatings: {
+    color: '#3af',
+    fontSize: 25,
   },
 });
