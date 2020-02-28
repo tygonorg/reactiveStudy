b712f19a04f8afe65d5e442e2a26d8b7186dc289
Trần Phước Hùng <admin@tygon.com.vn>
2020-02-28 22:46:42 +0700

    add nagavition

--- App.js
+++ App.js
@@ -1,78 +1,43 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-class Blink extends Component {
+// In App.js in a new project
 
-  componentDidMount() {
-    // Toggle the state every second
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
+import * as React from 'react';
+import {Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-  //state object
-  state = { isShowingText: true };
-
-  render() {
-    if (!this.state.isShowingText) {
-      return (
-        <Text style={styles.textShow}>{this.props.text1}</Text>
-      );
-    }
-    return (
-      <Text style={styles.textShow}>{this.props.text}</Text>
-    );
-  }
-}
-class Greeting extends Component {
-  render() {
-    return (
-      <Text style={styles.textgreatings}>Xin chào {this.props.name}!</Text>
-    );
-  }
-}
-class Bananas extends Component {
-  render() {
-    let pic = {
-      uri: 'https://znews-photo.zadn.vn/w660/Uploaded/ofh_btgazspf/2019_06_14/DSC_5505_1.jpg'
-    };
-    return (
-      <Image source={pic} style={{ width: 193, height: 110 }} />
-    );
-  }
-}
-
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Bananas pic='Xin chào' />
-      <Text style={styles.text}>Xin chào React Native</Text>
-      <Blink text='Đây là ba Hùng' text1='Ba chào các con!' />
-      <Greeting name='Mộc Mộc' />
-      <Greeting name='Viên Viên' />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgba(5, 165, 209, 0.25)',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#3498db',
-    fontSize: 17,
-  },
-  textgreatings: {
-    color: '#3af',
-    fontSize: 25,
-  },
-  textShow: {
-    color: 'red',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-});
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
